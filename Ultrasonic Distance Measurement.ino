ad1b9dc54cbdcb0ed6c0626b324b7a615e30493d
Mr-Eesa <155367487+Mr-Eesa@users.noreply.github.com>
2024-01-04 11:49:24 +0400

    Update Ultrasonic Distance Measurement.ino

--- Ultrasonic Distance Measurement.ino
+++ Ultrasonic Distance Measurement.ino
@@ -1,19 +1,15 @@
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-#include  <Wire.h>
-
-LiquidCrystal_I2C lcd(0x27,  16, 2);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define trig 8
 #define echo 7
 #define motor 10
 #define buzzer 6
 
-
 float distance;
-long  time;
-
-
+long time;
 
 void setup() {
   Serial.begin(9600);
@@ -23,48 +19,42 @@ void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
   lcd.init();
-  
+
   lcd.backlight();
-
-
 }
 
 void loop() {
-
-  
-  
-  digitalWrite(trig,HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trig,LOW);
+  digitalWrite(trig, LOW);
   time = pulseIn(echo, HIGH);
   distance = time * 0.34 / 20;
   Serial.print(distance);
-  Serial.println(" cm  :3");
+  Serial.println(" cm :3");
   delay(500);
   delay(1000);
-  
-  lcd.setCursor(0,0);
+
+  lcd.setCursor(0, 0);
   lcd.print("        ");
 
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print(distance);
   delay(300);
 
-
-
-  lcd.setCursor(0,1);
-  
+  lcd.setCursor(0, 1);
   lcd.print("bat-man");
 
+  if (distance > 6) {
+    analogWrite(motor, 90);
+  } else {
+    analogWrite(motor, 190);
+  }
 
-
-if (distance > 6 ){
-    analogWrite(motor, 90 );}
-  else analogWrite(motor, 190 );
-if (distance < 6 ){
-    digitalWrite(buzzer,HIGH);
+  if (distance < 6) {
+    digitalWrite(buzzer, HIGH);
     delay(300);
-    digitalWrite(buzzer,LOW);}
-  else digitalWrite(buzzer,LOW);
-
+    digitalWrite(buzzer, LOW);
+  } else {
+    digitalWrite(buzzer, LOW);
+  }
 }
